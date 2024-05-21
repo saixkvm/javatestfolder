5cadd8f76402b48c9d0c59c5646028591afc724d
Sai M <saikarthikvm@gmail.com>
2024-05-21 12:11:19 -0400

    Added new file

---
+++ tester.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) 
+    {
+        String hi = "Hi";
+        for (int i = 0; i < 2; i++){
+            System.out.println(hi);
+        }
+    }
+}
